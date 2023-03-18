36dd937bfa3ec6c0dbbaee2ed6622cd09af920c1
ianho1 <ianho@brandeis.edu>
2023-03-18 17:15:05 -0400

    Ian's part done

--- ca01/gptwebapp.py
+++ ca01/gptwebapp.py
@@ -83,6 +83,9 @@ def compile():
     <br>
     <br>
     <a href="{url_for('bisrat')}">Bisrats Project - Generate a Poem</a>
+    <br>
+    <br>
+    <a href="{url_for('ian')}">Ians Project - Generate a Joke</a>
     '''
 
 @app.route('/team')
@@ -188,6 +191,33 @@ def bisrat():
             <p><input type=submit value="get response">
         </form>
          <a href={url_for('index')}>Return to Home Page</a>
+        '''
+
+# Added by Ian
+@app.route('/ian', methods=['GET', 'POST'])
+def ian():
+    ''' handle a get request by sending a form 
+        and a post request by returning the GPT response
+    '''
+    if request.method == 'POST':
+        prompt = request.form['prompt']
+        answer = gptAPI.getJoke(prompt)
+        return f'''
+        <h1>Jokes</h1>
+        <h2>What type of joke do you want?</h2>
+        <div style="border:thin solid black">{answer}</div>
+        <p><p>
+        <a href={url_for('ian')}> make another joke</a>
+        '''
+    else:
+        return f'''
+        <h1>Joke Generator </h1>
+        What kind of joke do you want?
+        <form method="post">
+            <textarea name="prompt"></textarea>
+            <p><input type=submit value="get response">
+        </form>
+        <a href={url_for('index')}>Return to Home Page</a>
         '''
 
 # hickey
